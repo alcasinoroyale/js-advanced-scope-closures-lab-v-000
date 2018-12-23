035926ade841c1abfbdcda9407638f13b5b374a3
Alex Zdatny <alexzdatny@gmail.com>
2018-12-23 18:52:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 function produceDrivingRange(blockRange) {
-  return function(tripRange) {
-    return tripRange * blockRange;
-  };
+  return function (startingBlock, endingBlock) {
+    let start =
+    let end =
+    let distance = end - start;
+    let difference = distance - blockRange;
+
+    if (difference > 0) {
+      return `${Math.abs(difference)} blocks out of range`
+    } else {
+      return `within range by ${Math.abs(difference)}`
+    }
+  }
 }
